b285c74192b0e8f30af62acc48735a85a9917436
Mariano <marianocapiliku@gmail.com>
2023-11-14 02:12:37 +0100

    Get user Information from GITHUB API Usinf useEffect Hoock to do a Http request to GITHUB API and get all needed user information

---
+++ src/components/UserInformation/UserContent.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { GitIcon } from "././Icons/GitIcon";
+import { CompanyIcon } from "./Icons/CompanyIcon";
+import { FellowsIcon } from "./Icons/FellowsIcon";
+import { GitLinkIcon } from "./Icons/GitLinkIcon";
+
+import "./UserContent.css";
+
+export function UserContent() {
+  const [userInfo, setInfo] = useState({
+    userName: "",
+    userBio: "",
+    userGit: "",
+    userCompany: "",
+    userFellows: 0,
+  });
+
+  useEffect(() => {
+    
+    async function getUserInfo() {
+      const user = await fetch("https://api.github.com/users/mariano1994");
+      const userData = await user.json();
+      setInfo({
+        userName: userData.name,
+        userBio: userData.bio,
+        userGit: userData.login,
+        userCompany: userData.company,
+        userFellows: userData.followers,
+      });
+    }
+
+    getUserInfo();
+
+  }, []);
+
+  return (
+    <>
+      <div className="title">
+        <h2>{userInfo.userName}</h2>
+
+        <div className="git-link">
+          <a href="https://github.com/mariano1994" target="_blanck">
+            Github
+          </a>
+          <GitLinkIcon />
+        </div>
+      </div>
+
+      <div className="content">
+        <p>{userInfo.userBio}</p>
+      </div>
+
+      <div className="user-info">
+        <div className="user-name">
+          <GitIcon />
+          <span>{userInfo.userGit}</span>
+        </div>
+
+        <div className="user-company">
+          <CompanyIcon />
+          <span>{userInfo.userCompany}</span>
+        </div>
+
+        <div className="user-fellows">
+          <FellowsIcon />
+          <span>{userInfo.userFellows} followers</span>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/UserInformation/UserInformation.tsx
+++ src/components/UserInformation/UserInformation.tsx
@@ -1,47 +1,14 @@
 import "./UserInformation.css";
 import { UserAvatar } from "./UserAvatar";
-import { GitIcon } from "././Icons/GitIcon";
-import { CompanyIcon } from "./Icons/CompanyIcon";
-import { FellowsIcon } from "./Icons/FellowsIcon";
-import { GitLinkIcon } from "./Icons/GitLinkIcon";
+import { UserContent } from "./UserContent";
+
 export function UserInformation() {
   return (
     <>
       <div className="user-wrapper">
         <UserAvatar />
         <div className="user-informations">
-          <div className="title">
-            <h2>Mariano Capiliku</h2>
-
-            <div className="git-link">
-              <a href="https://github.com/mariano1994" target='_blanck'>Github</a>
-              <GitLinkIcon />
-            </div>
-          </div>
-
-          <div className="content">
-            <p>
-              I am a dedicated and passionate Software Engineer with background
-              in front-end development, specializing in ReactJS and TypeScript.
-            </p>
-          </div>
-
-          <div className="user-info">
-            <div className="user-name">
-              <GitIcon />
-              <span>Mariano1994</span>
-            </div>
-
-            <div className="user-company">
-              <CompanyIcon />
-              <span>Pcsnet</span>
-            </div>
-
-            <div className="user-fellows">
-              <FellowsIcon />
-              <span>3 fellows</span>
-            </div>
-          </div>
+        <UserContent/>
         </div>
       </div>
     </>
