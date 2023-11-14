661cdfab968b57a39eb572953551e35ff8ca079d
Mariano <marianocapiliku@gmail.com>
2023-11-14 16:34:37 +0100

    Use useState Hook to control the posts list

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 import { UserInformation } from "./components/UserInformation/UserInformation";
 import { SearchBar } from "./components/SearchBar/SearchBar";
 import { Posts } from "./components/Posts/Posts";
+import { useState } from "react";
 
-const posts = [
+const initialsPosts = [
   {
     id: Math.random(),
     title: "JavaScript data types and data structures",
@@ -27,7 +28,7 @@ const posts = [
     content:
       "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available...",
     publishedAt: new Date(),
-  },  
+  },
 
   {
     id: Math.random(),
@@ -35,7 +36,7 @@ const posts = [
     content:
       "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available...",
     publishedAt: new Date(),
-  }, 
+  },
 
   {
     id: Math.random(),
@@ -43,18 +44,40 @@ const posts = [
     content:
       "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available...",
     publishedAt: new Date(),
-  }, 
+  },
 
+  {
+    id: Math.random(),
+    title: "JavaScript data types and data structures",
+    content:
+      "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available...",
+    publishedAt: new Date(),
+  },
+  {
+    id: Math.random(),
+    title: "JavaScript data types and data structures",
+    content:
+      "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available...",
+    publishedAt: new Date(),
+  },
+  {
+    id: Math.random(),
+    title: "JavaScript data types and data structures",
+    content:
+      "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available...",
+    publishedAt: new Date(),
+  },
 ];
 
 export function App() {
+  const [posts, setPosts] = useState(initialsPosts);
   return (
     <>
       <Header />
       <div className="wrapper-content">
         <UserInformation />
       </div>
-      <SearchBar posts={posts}/>
+      <SearchBar posts={posts} />
       <Posts posts={posts} />
     </>
   );
