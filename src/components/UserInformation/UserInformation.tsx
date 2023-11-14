310ee476ed4e8e3b53b881bd019b9d2cba318ff2
Mariano <marianocapiliku@gmail.com>
2023-11-14 01:33:17 +0100

    Create UserInformation component

--- src/components/UserInformation/UserInformation.tsx
+++ src/components/UserInformation/UserInformation.tsx
@@ -1,24 +1,49 @@
-
-import './UserInformation.css'
-import {UserAvatar} from './UserAvatar'
+import "./UserInformation.css";
+import { UserAvatar } from "./UserAvatar";
+import { GitIcon } from "././Icons/GitIcon";
+import { CompanyIcon } from "./Icons/CompanyIcon";
+import { FellowsIcon } from "./Icons/FellowsIcon";
+import { GitLinkIcon } from "./Icons/GitLinkIcon";
 export function UserInformation() {
   return (
     <>
       <div className="user-wrapper">
-        <UserAvatar/>
-        <div className='user-informations'>
-        
+        <UserAvatar />
+        <div className="user-informations">
           <div className="title">
             <h2>Mariano Capiliku</h2>
-            <a href="">Github</a>
+
+            <div className="git-link">
+              <a href="https://github.com/mariano1994" target='_blanck'>Github</a>
+              <GitLinkIcon />
+            </div>
           </div>
 
           <div className="content">
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita, ullam. Repellendus repellat error quo laborum inventore at, voluptates aliquam perspiciatis, dicta adipisci doloribus quia. Asperiores saepe sequi eum expedita officiis?</p>
+            <p>
+              I am a dedicated and passionate Software Engineer with background
+              in front-end development, specializing in ReactJS and TypeScript.
+            </p>
           </div>
 
+          <div className="user-info">
+            <div className="user-name">
+              <GitIcon />
+              <span>Mariano1994</span>
+            </div>
+
+            <div className="user-company">
+              <CompanyIcon />
+              <span>Pcsnet</span>
+            </div>
+
+            <div className="user-fellows">
+              <FellowsIcon />
+              <span>3 fellows</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
